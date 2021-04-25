c6ef14f86d0850d0772f24169df5ba24bedd4f2f
mgonawala <mohini.gonawala90@gmail.com>
2021-04-25 13:01:51 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+
+  products: Product[];
+
+  constructor(){
+    this.products =
+     [
+       new Product('MYSHOES','Black Runnig Shoes',"assets/images/products/black-shoes.jpg",["Men","Shoes","Running Shoes"],50.12),
+       new Product('NEATOJACKEST','Blue Jacket',"assets/images/products/blue-jacket.jpg",["Men","Apparel","Jackets & Vests"],60.45),
+       new Product('NICEHAT','Hat',"assets/images/products/black-hat.jpg",["Men","Accessories","Hats"],29.99),
+     ];
+  }
+
+  productWasSelected(product: Product){
+    console.log('Product Clicked:',product);
+  }
 }
